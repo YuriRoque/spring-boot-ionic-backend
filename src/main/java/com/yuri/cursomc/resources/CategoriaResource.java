40bfc599cbcb0eea02a532d80a00c6311a903523
Yuri Benvenutti <yrbenvenutti@gmail.com>
2021-11-02 11:34:10 -0400

    Testando a primeira classe de dominio - Categoria

--- src/main/java/com/yuri/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/yuri/cursomc/resources/CategoriaResource.java
@@ -1,5 +1,10 @@
 package com.yuri.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.yuri.cursomc.domain.Categoria;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,8 +14,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class CategoriaResource {
   
   @RequestMapping(method = RequestMethod.GET)
-  public String listar() {
-    return "REST está funcionando";
+  public List<Categoria> listar() {
+
+    Categoria cat1 = new Categoria(1, "Informática");
+    Categoria cat2 = new Categoria(2, "Escritório");
+
+    List<Categoria> lista = new ArrayList<>();
+    lista.add(cat1);
+    lista.add(cat2);
+
+    return lista;
   }
 
 }
