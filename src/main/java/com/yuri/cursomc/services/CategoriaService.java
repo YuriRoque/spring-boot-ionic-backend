230ac4110dd37965490505d0bd9167c22dbbd2da
Yuri Benvenutti <yrbenvenutti@gmail.com>
2021-11-07 15:27:27 -0400

    Ajustes o endpoint /categorias/{id}

--- src/main/java/com/yuri/cursomc/services/CategoriaService.java
+++ src/main/java/com/yuri/cursomc/services/CategoriaService.java
@@ -4,6 +4,7 @@ import java.util.Optional;
 
 import com.yuri.cursomc.domain.Categoria;
 import com.yuri.cursomc.repositories.CategoriaRepository;
+import com.yuri.cursomc.services.exceptions.ObjectNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -15,7 +16,12 @@ public class CategoriaService {
   private CategoriaRepository repo;
 
   public Categoria buscar(Integer id) {
+
     Optional<Categoria> obj = repo.findById(id);
-    return obj.orElse(null);
+
+    return obj.orElseThrow(
+        () -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName())
+    );
   }
+
 }
