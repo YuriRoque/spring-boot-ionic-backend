f772cee856bc8f751922e0772a36ca44ee756209
Yuri Benvenutti <yrbenvenutti@gmail.com>
2021-11-07 14:55:51 -0400

    Produto e associacao muitos para muitos

--- src/main/java/com/yuri/cursomc/domain/Produto.java
+++ src/main/java/com/yuri/cursomc/domain/Produto.java
@@ -8,31 +8,39 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 
 @Entity
-public class Categoria implements Serializable {
+public class Produto implements Serializable {
 
   private static final long serialVersionUID = 1L;
-
+  
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Integer id;
   private String nome;
+  private Double preco;
 
-  @ManyToMany(mappedBy = "categorias")
-  private List<Produto> produtos = new ArrayList<>();
+  @ManyToMany
+  @JoinTable(name = "PRODUTO_CATEGORIA",
+            joinColumns = @JoinColumn(name = "produto_id"),
+            inverseJoinColumns = @JoinColumn(name = "categoria_id")
+  )
+  private List<Categoria> categorias = new ArrayList<>();
 
-  public Categoria() {
-  }  
+  public Produto() {
+  }
 
-  public Categoria(Integer id, String nome) {
+  public Produto(Integer id, String nome, Double preco) {
     this.id = id;
     this.nome = nome;
+    this.preco = preco;
   }
 
   public Integer getId() {
-    return this.id;
+    return id;
   }
 
   public void setId(Integer id) {
@@ -40,19 +48,27 @@ public class Categoria implements Serializable {
   }
 
   public String getNome() {
-    return this.nome;
+    return nome;
   }
 
   public void setNome(String nome) {
     this.nome = nome;
   }
 
-  public List<Produto> getProdutos() {
-    return produtos;
+  public Double getPreco() {
+    return preco;
   }
 
-  public void setProdutos(List<Produto> produtos) {
-    this.produtos = produtos;
+  public void setPreco(Double preco) {
+    this.preco = preco;
+  }
+
+  public List<Categoria> getCategorias() {
+    return categorias;
+  }
+
+  public void setCategorias(List<Categoria> categorias) {
+    this.categorias = categorias;
   }
 
   @Override
@@ -71,13 +87,13 @@ public class Categoria implements Serializable {
       return false;
     if (getClass() != obj.getClass())
       return false;
-    Categoria other = (Categoria) obj;
+    Produto other = (Produto) obj;
     if (id == null) {
       if (other.id != null)
         return false;
     } else if (!id.equals(other.id))
       return false;
     return true;
-  } 
-  
+  }
+
 }
---
+++ src/main/java/com/yuri/cursomc/repositories/ProdutoRepository.java
@@ -0,0 +1,11 @@
+package com.yuri.cursomc.repositories;
+
+import com.yuri.cursomc.domain.Produto;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
+  
+}
