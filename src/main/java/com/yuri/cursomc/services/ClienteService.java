d78cb88b174a7fc7021ce007122b41e9f6c538e6
Yuri Benvenutti <yrbenvenutti@gmail.com>
2021-11-30 16:11:50 -0400

    Ajustando e testando a delecao de Cliente

--- src/main/java/com/yuri/cursomc/services/ClienteService.java
+++ src/main/java/com/yuri/cursomc/services/ClienteService.java
@@ -65,7 +65,7 @@ public class ClienteService {
     try {
       clienteRepository.deleteById(id);
     } catch (DataIntegrityException e) {
-      throw new DataIntegrityException("Não é possível excluir porque há entidades relacionadas");
+      throw new DataIntegrityException("Não é possível excluir porque há pedidos relacionados");
     }
   }
   
