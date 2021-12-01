09ec9be03853df0861adb01494ee476bebef2fe1
Yuri Benvenutti <yrbenvenutti@gmail.com>
2021-12-01 12:38:55 -0400

    Validacao customizada: email nao repetido na atualizacao de cliente

--- src/main/java/com/yuri/cursomc/dto/ClienteDTO.java
+++ src/main/java/com/yuri/cursomc/dto/ClienteDTO.java
@@ -6,9 +6,11 @@ import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
 
 import com.yuri.cursomc.domain.Cliente;
+import com.yuri.cursomc.services.validation.ClienteUpdate;
 
 import org.hibernate.validator.constraints.Length;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable {
   
   private static final long serialVersionUID = 1L;
