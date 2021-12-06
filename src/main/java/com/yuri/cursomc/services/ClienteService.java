704e944fdc0cfb1f52f2ef68c8afc2ace6742542
Yuri Benvenutti <yrbenvenutti@gmail.com>
2021-12-06 11:39:12 -0400

    Adicionando senha a Cliente

--- src/main/java/com/yuri/cursomc/services/ClienteService.java
+++ src/main/java/com/yuri/cursomc/services/ClienteService.java
@@ -18,12 +18,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class ClienteService {
   
+  @Autowired
+  private BCryptPasswordEncoder pe;
+
   @Autowired
   private ClienteRepository clienteRepository;
 
@@ -84,7 +88,7 @@ public class ClienteService {
   }
 
   public Cliente fromDTO(ClienteDTO objDto) {
-    return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null);
+    return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null, null);
   }
 
   private void updateData(Cliente newObj, Cliente obj) {
@@ -94,9 +98,10 @@ public class ClienteService {
 
   public Cliente fromDTO(ClienteNewDTO objDto) {
     Cliente cli = new Cliente(null, objDto.getNome(), objDto.getEmail(), objDto.getCpfOuCnpj(),
-        TipoCliente.toEnum(objDto.getTipo()));
+        TipoCliente.toEnum(objDto.getTipo()), pe.encode(objDto.getSenha()));
     Cidade cid = new Cidade(objDto.getCidadeId(), null, null);
-    Endereco end = new Endereco(null, objDto.getLogradouro(), objDto.getNumero(), objDto.getComplemento(),
+    Endereco end = new Endereco(null, objDto.getLogradouro(), objDto.getNumero(),
+        objDto.getComplemento(),
         objDto.getBairro(), objDto.getCep(), cli, cid);
 
     cli.getEnderecos().add(end);
--- src/main/java/com/yuri/cursomc/services/DBService.java
+++ src/main/java/com/yuri/cursomc/services/DBService.java
@@ -28,10 +28,14 @@ import com.yuri.cursomc.repositories.PedidoRepository;
 import com.yuri.cursomc.repositories.ProdutoRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class DBService {
+
+  @Autowired
+  private BCryptPasswordEncoder pe;
 
   @Autowired
   private CategoriaRepository categoriaRepository;
@@ -118,7 +122,7 @@ public class DBService {
     estadoRepository.saveAll(Arrays.asList(est1, est2));
     cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
 
-    Cliente cli1 = new Cliente(null, "Maria Silva", "yrbenvenutti@hotmail.com", "36378912377", TipoCliente.PESSOAFISICA);
+    Cliente cli1 = new Cliente(null, "Maria Silva", "yrbenvenutti@hotmail.com", "36378912377", TipoCliente.PESSOAFISICA, pe.encode("123"));
 
     cli1.getTelefones().addAll(Arrays.asList("27363323", "93838393"));
 
