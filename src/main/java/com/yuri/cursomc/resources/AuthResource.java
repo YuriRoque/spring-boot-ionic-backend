977b3c4bd8cdc1546f204d74ad3ac6c4049a29d7
Yuri Benvenutti <yrbenvenutti@gmail.com>
2021-12-10 11:30:22 -0400

    Expondo o header Authorization(problema de Cors)

--- src/main/java/com/yuri/cursomc/resources/AuthResource.java
+++ src/main/java/com/yuri/cursomc/resources/AuthResource.java
@@ -33,6 +33,7 @@ public class AuthResource {
     String token = jwtUtil.generateToken(user.getUsername());
 
     response.addHeader("Authorization", "Bearer " + token);
+    response.addHeader("access-control-expose-headers", "Authorization");
 
     return ResponseEntity.noContent().build();
 
--- src/main/java/com/yuri/cursomc/security/JWTAuthenticationFilter.java
+++ src/main/java/com/yuri/cursomc/security/JWTAuthenticationFilter.java
@@ -59,6 +59,7 @@ public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilte
     String token = jwtUtil.generateToken(username);
 
     res.addHeader("Authorization", "Bearer " + token);
+    res.addHeader("access-control-expose-headers", "Authorization");
 
   }
   
